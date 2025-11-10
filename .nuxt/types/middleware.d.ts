f2f81e1faaad6d773c05433599a57f227f138706
N1MRODve <dieterlorenzo@gmail.com>
2025-11-10 22:19:08 +0100

    Deleted .DS_Store

--- .nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = "admin-auth" | "auth-role" | "auth-terapeuta" | "auth" | "role-coordinadora"
-declare module 'nuxt/app' {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
